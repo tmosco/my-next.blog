a018beb7bcb0a520aecc7933a8b8603426bcde15
tmosco <tmosco_tayo@yahoo.com>
2021-04-19 22:53:03 +0100

    user can view all post

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,18 +1,39 @@
-
+import AllPosts from "../../components/posts/all-posts";
 
 const AllPostsPage = () => {
-    return (
-        <div>
+
+    const DUMMY_DATA = [
+        { 
+            slug:"getting-started-with-nextJs",
+             title:"Getting started with next js",
+              image:"getting-started-with-nextjs.png", 
+              excerpt:"This is a framework of the react and nextjs", 
+              date:"2022-02-23", },
+        { 
+            slug:"getting-started-with-nextJs2",
+             title:"Getting started with next js",
+              image:"getting-started-with-nextjs.png", 
+              excerpt:"This is a framework of the react and nextjs", 
+              date:"2022-02-23", },
+        { 
+            slug:"getting-started-with-nextJs3",
+             title:"Getting started with next js",
+              image:"getting-started-with-nextjs.png", 
+              excerpt:"This is a framework of the react and nextjs", 
+              date:"2022-02-23", },
+        { 
+            slug:"getting-started-with-nextJs4",
+             title:"Getting started with next js",
+              image:"getting-started-with-nextjs.png", 
+              excerpt:"This is a framework of the react and nextjs", 
+              date:"2022-02-23" ,},
             
-        </div>
-    );
+            ];
+
+
+
+    
+  return <AllPosts  posts={DUMMY_DATA}/>;
 };
 
 export default AllPostsPage;
-
-
-
-
-
-
-
